8f7279e4cc1f6a95ea5553ec8cbadbd4174a4a48
psdewar2 <psdewar2@live.com>
2016-01-24 01:09:19 -0500

    Added facebook login functionality

---
+++ iOS/tutorMe/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  tutorMe
+//
+//  Created by Peyt Spencer Dewar on 1/23/16.
+//  Copyright © 2016 PSD. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+
+#endif /* Bridging_Header_h */
---
+++ iOS/tutorMe/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  tutorMe
+//
+//  Created by Peyt Spencer Dewar on 1/23/16.
+//  Copyright © 2016 PSD. All rights reserved.
+//
+
+#ifndef FacebookTutorial_Bridging_Header_h
+#define FacebookTutorial_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+
+#endif /* Header_h */
